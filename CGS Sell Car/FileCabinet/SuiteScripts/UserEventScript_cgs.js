a56fa9f714344faed0b2ded5009bea7604aedc7d
pprasadnetsuite <37029223+pprasadnetsuite@users.noreply.github.com>
2018-03-08 09:45:07 +1100

    CGS

---
+++ CGS Sell Car/FileCabinet/SuiteScripts/UserEventScript_cgs.js
@@ -0,0 +1,61 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+define([],
+
+function() {
+   
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {string} scriptContext.type - Trigger type
+     * @param {Form} scriptContext.form - Current form
+     * @Since 2015.2
+     */
+	function beforeLoad(scriptContext) {
+		
+		if (scriptContext.type === scriptContext.UserEventType.CREATE) { // Is it a CREATE event ?
+			
+			var customRecord = scriptContext.newRecord;
+			
+			customRecord.setValue({fieldId: 'custrecord_cgs_model', value: 'Tesla Model S'}); // Assigning a default value to 'MODEL' field
+		}
+	}
+
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {Record} scriptContext.oldRecord - Old record
+     * @param {string} scriptContext.type - Trigger type
+     * @Since 2015.2
+     */
+    function beforeSubmit(scriptContext) {
+
+    }
+
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {Record} scriptContext.oldRecord - Old record
+     * @param {string} scriptContext.type - Trigger type
+     * @Since 2015.2
+     */
+    function afterSubmit(scriptContext) {
+
+    }
+
+    return {
+        beforeLoad: beforeLoad,
+        beforeSubmit: beforeSubmit,
+        afterSubmit: afterSubmit
+    };
+    
+});
